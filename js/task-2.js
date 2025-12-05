2fbec350d915a1271abd09e8139bf542a3726b76
rubin <rubin666666666@gmail.com>
2025-12-05 10:52:15 +0200

    Add .gitignore and remove .history folder

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,7 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+const gallery = document.querySelector('.gallery');
+const markup = images.map(({url, alt}) => 
+  `<li><img src="${url}" alt="${alt}" width="300"></li>`).join('');
+gallery.insertAdjacentHTML('beforeend', markup);
